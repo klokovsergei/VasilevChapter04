6162b1b04c2cbb11d4f285ac7ab373aea98218ce
Sergei Klokov <s2584835@ya.ru>
2024-01-16 23:37:51 +0300

    feat: task09 - code witch create array

---
+++ Task09/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, в которой создается и инициализируется дву­мерный числовой массив.
+// Затем из этого массива удаляется строка и столбец (создается новый массив,
+// в котором по сравнению с исход­ным удалена одна строка и один столбец).
+// Индекс удаляемой строки и индекс удаляемого столбца определяется с помощью генератора слу­чайных чисел.
+
+void ShowArr(int[,] nums)
+{
+    for (int i = 0; i < nums.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < nums.GetLength(1); j++)
+        {
+            Console.Write(nums[i,j] + " ");
+        }
+        Console.Write("]\n");
+    }
+}
+
+Console.Write("Введите размерность двумерного массива [ X ,   ]: ");
+int firstSize = int.Parse(Console.ReadLine());
+Console.Write("Введите размерность двумерного массива [   , X ]: ");
+int secondSize = int.Parse(Console.ReadLine());
+
+//создаем двумерный массив
+int[,] array = new int[firstSize, secondSize];
+Random random = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = random.Next(10,100);
+    }
+}
+
+ShowArr(array);
+
+//определим какая строка и столбец будут удалены
+int delRow = random.Next(0, array.GetLength(0));
+int delCol = random.Next(0, array.GetLength(1));
+
+Console.WriteLine("Будут удалены строка - {0} и колонка - {1}.", delRow, delCol);
+
+//удаление строки и столбца выбранных случайным образом
